1b1a34f3954c5a1647fae926046b3ec05e9b18e9
Dylan <dj0759@gmail.com>
2017-08-18 19:12:34 -0700

    Changed sign up form

--- app.js
+++ app.js
@@ -53,8 +53,24 @@ app.use('/users', users);
 app.use('/newQuestion', newQuestion);
 app.use('/signup', signup);
 app.use('/login', login);
-app.use('/dashboard', dashboard); 
+app.use('/dashboard', dashboard);
 
+app.use(function(req, res, next) {
+    if (req.session && req.session.user) {
+        User.findOne({ email: req.session.user.email }, function(err, user) {
+            if (user) {
+                req.user = user;
+                delete req.user.password; // delete the password from the session
+                req.session.user = user; //refresh the session value
+                res.locals.user = user;
+            }
+            // finishing processing the middleware and run the route
+            next();
+        });
+    } else {
+        next();
+    }
+});
 
 var pathfinderUI = require('pathfinder-ui');
 //pathfinder
--- controllers/signup.controller.js
+++ controllers/signup.controller.js
@@ -1,21 +1,21 @@
 var user = require('../models/user.model');
 
 var crypto = require('crypto'),
-algorithm = 'aes-256-ctr',
-password = 'abcdefg';
+    algorithm = 'aes-256-ctr',
+    password = 'abcdefg';
 
-function encrypt(text){
-var cipher = crypto.createCipher(algorithm,password)
-var crypted = cipher.update(text,'utf8','hex')
-crypted += cipher.final('hex');
-return crypted;
+function encrypt(text) {
+    var cipher = crypto.createCipher(algorithm, password)
+    var crypted = cipher.update(text, 'utf8', 'hex')
+    crypted += cipher.final('hex');
+    return crypted;
 };
 
-function decrypt(text){
-var decipher = crypto.createDecipher(algorithm,password)
-var dec = decipher.update(text,'hex','utf8')
-dec += decipher.final('utf8');
-return dec;
+function decrypt(text) {
+    var decipher = crypto.createDecipher(algorithm, password)
+    var dec = decipher.update(text, 'hex', 'utf8')
+    dec += decipher.final('utf8');
+    return dec;
 };
 
 exports.getUserTemplate = function(req, res, next) {
@@ -32,28 +32,32 @@ exports.addNewUser = function(req, res, next) {
         email: req.body.email,
         createdAt: currentDate
     });
-    user.findOne({ username: req.body.username}, function (err, username) {
+    user.findOne({ username: req.body.username }, function(err, username) {
         if (err) {
             next(err);
         }
         if (username) {
-            res.send("USERNAME ALREADY EXISTS CLICK BACK AND TRY AGAIN");
+            res.render('newUser', { error: "USERNAME ALREADY EXISTS" });
         } else {
-            user.findOne({ email: req.body.email}, function (err, email) {
-            if (err){
-                next(err);
-            } if (email){
-                res.send("EMAIL ALREADY EXISTS CLICK BACK AND TRY AGAIN");
-            } else {
-                userToAdd.save(function(err, payload) {
-                    if (err) {
-                        next(err);
-                    } {
-                        return res.send("You have successfully created a User");
-                    }
-            }); // end of save
-            }})
-}})};
+            user.findOne({ email: req.body.email }, function(err, email) {
+                if (err) {
+                    next(err);
+                }
+                if (email) {
+                    res.render('newUser', { error: "EMAIL ALREADY EXISTS" });
+                } else {
+                    userToAdd.save(function(err, payload) {
+                        if (err) {
+                            next(err);
+                        } {
+                            return res.send("You have successfully created a User");
+                        }
+                    }); // end of save
+                }
+            })
+        }
+    })
+};
 /*
 var crypto = require('crypto'),
 algorithm = 'aes-256-ctr',
